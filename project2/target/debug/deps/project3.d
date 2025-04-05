96ddab05c94e09ea52356c3ed819f6c6c1fa6866
Sze Ying <bloom9334@hotmail.com>
2025-04-05 17:44:40 +0800

    feat: add code for prj 2

---
+++ project2/target/debug/deps/project3.d
@@ -0,0 +1,5 @@
+C:\Users\Admin\Documents\Coding\Solana\project2\target\debug\deps\project3.exe: src\main.rs
+
+C:\Users\Admin\Documents\Coding\Solana\project2\target\debug\deps\project3.d: src\main.rs
+
+src\main.rs:
---
+++ project2/target/debug/project3.d
@@ -0,0 +1,1 @@
+C:\Users\Admin\Documents\Coding\Solana\project2\target\debug\project3.exe: C:\Users\Admin\Documents\Coding\Solana\project2\src\main.rs
